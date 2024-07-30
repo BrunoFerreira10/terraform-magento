823c7d361efe53b60d1c3675ad52be3d570204e5
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-07-30 16:17:10 -0300

    Fix

--- 4c-EC2 Update/ec2-update/main.tf
+++ 4c-EC2 Update/ec2-update/main.tf
@@ -16,14 +16,41 @@ data "terraform_remote_state" "remote-state-rds" {
   }
 }
 
-# data "terraform_remote_state" "remote-state-efs" {
-#   backend = "s3"
-#   config = {
-#     region = var.regiao
-#     bucket = var.remote-state-bucket
-#     key    = "EFS/terraform.tfstate"
-#   }
-# }
+data "terraform_remote_state" "remote-state-opensearch" {
+  backend = "s3"
+  config = {
+    region = var.regiao
+    bucket = var.remote-state-bucket
+    key    = "Open Search/terraform.tfstate"
+  }
+}
+
+data "terraform_remote_state" "remote-state-elasticcache" {
+  backend = "s3"
+  config = {
+    region = var.regiao
+    bucket = var.remote-state-bucket
+    key    = "Elastic Cache/terraform.tfstate"
+  }
+}
+
+data "terraform_remote_state" "remote-state-s3-static-files" {
+  backend = "s3"
+  config = {
+    region = var.regiao
+    bucket = var.remote-state-bucket
+    key    = "S3 Static Files/terraform.tfstate"
+  }
+}
+
+data "terraform_remote_state" "remote-state-efs" {
+  backend = "s3"
+  config = {
+    region = var.regiao
+    bucket = var.remote-state-bucket
+    key    = "EFS/terraform.tfstate"
+  }
+}
 
 data "terraform_remote_state" "remote-ami" {
   backend = "s3"
