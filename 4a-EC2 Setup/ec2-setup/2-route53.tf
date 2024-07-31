6df5c0fcbe6d26a494bf90d457543570a962c3bd
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-07-31 11:33:54 -0300

    Fix

--- 4a-EC2 Setup/ec2-setup/2-route53.tf
+++ 4a-EC2 Setup/ec2-setup/2-route53.tf
@@ -4,35 +4,24 @@ data "aws_route53_zone" "hosted_zone" {
   private_zone = false
 }
 
-# DNS Record - A - record_setup
-resource "aws_route53_record" "record_setup" {
+# DNS Record - Tipo: A
+resource "aws_route53_record" "record_A" {
 
   allow_overwrite = true
   zone_id         = data.aws_route53_zone.hosted_zone.zone_id
-  name            = "setup.${var.domain-base}"
+  name            = var.domain-base
   type            = "A"
   ttl             = 60
   records         = [aws_instance.ec2-setup.public_ip]
 }
 
-# // DNS Record - A
-# resource "aws_route53_record" "record_A" {
+# DNS Record - Tipo: A - record_www
+resource "aws_route53_record" "record_WWW" {
 
-#   allow_overwrite = true
-#   zone_id         = data.aws_route53_zone.hosted_zone.zone_id
-#   name            = var.domain-base
-#   type            = "A"
-#   ttl             = 60
-#   records         = [aws_instance.ec2-setup.public_ip]
-# }
-
-// DNS Record - CNAME - www
-# resource "aws_route53_record" "record_WWW" {
-
-#   allow_overwrite = true
-#   zone_id         = data.aws_route53_zone.hosted_zone.zone_id
-#   name            = "www.${var.domain-base}"
-#   type            = "A"
-#   ttl             = 60
-#   records         = [aws_instance.ec2-setup.public_ip]
-# }
+  allow_overwrite = true
+  zone_id         = data.aws_route53_zone.hosted_zone.zone_id
+  name            = "www.${var.domain-base}"
+  type            = "A"
+  ttl             = 60
+  records         = [aws_instance.ec2-setup.public_ip]
+}
